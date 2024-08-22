f99a2197059771bac7054fb6eb6dc8a1ba382764
Luca <l.c.hagemans@student.tudelft.nl>
2024-08-22 23:39:36 +0200

    Parse most streamed song in terms of time

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,24 @@
+use std::collections::{HashMap, HashSet};
 use std::fs::{self, File};
-use std::path::Path;
+use std::iter::zip;
+use std::path::{Path, PathBuf};
+use std::time::{Duration, Instant};
 
-use crate::streamingdata::StreamingData;
+use streamingdata::Track;
+
+use crate::streamingdata::{StreamingData, StreamingDataRaw};
 
 mod streamingdata;
 
-fn main() {
+fn main() -> Result<(), ()> {
     // General idea:
-    // 1. Get all files in the ./../data/ directory
+    // 1. Get all files in the ./data/ directory
     // 2. Parse these files into a vec of structs
     // 3. Use this vector to store data for each track in a hashmap
     // 4. Somehow get interesting data from this hashmap
 
-    // Part 1: get all files in the ./../data/ directory
-    let directory = Path::new("./../data/");
-    let mut paths = Vec::new();
-    for entry in fs::read_dir(directory).unwrap() {
-        let entry = entry.unwrap();
-        let path = entry.path();
-        paths.push(path);
-    }
-    // dbg!(files); // This works
+    // Part 1: get all files in the ./data/ directory
+    let paths = get_directories().ok_or(())?;
 
     // let example = json!([
     //     {"ts":"2022-12-09T22:28:37Z","username":"swaggerboy02","platform":"android","ms_played":1596,"conn_country":"NL","ip_addr_decrypted":"89.205.128.220","user_agent_decrypted":null,"master_metadata_track_name":"Out of Time","master_metadata_album_artist_name":"The Weeknd","master_metadata_album_album_name":"Dawn FM","spotify_track_uri":"spotify:track:2SLwbpExuoBDZBpjfefCtV","episode_name":null,"episode_show_name":null,"spotify_episode_uri":null,"reason_start":"backbtn","reason_end":"backbtn","shuffle":false,"skipped":true,"offline":false,"offline_timestamp":1670624916,"incognito_mode":false},
@@ -32,29 +30,52 @@ fn main() {
     // dbg!(example_struct);
 
     // Part 2: parse these files into a vec of structs
-    let mut all_streams: Vec<StreamingData> = Vec::new();
+    let mut all_streams: Vec<StreamingDataRaw> = Vec::new();
+
+    let start_overall = Instant::now();
+
     // let first_file = File::open(&files[0]).unwrap();
     for path in paths {
         println!("Parsing file: {:?}", path);
-        let file = File::open(&path).unwrap();
-        let json: serde_json::Value = serde_json::from_reader(file).unwrap();
-        let mapped: Vec<StreamingData> = json
-            .as_array()
-            .unwrap()
+        let start = Instant::now();
+        let file = if let Ok(file) = File::open(&path) {
+            file
+        } else {
+            continue;
+        };
+        let reader = std::io::BufReader::new(file);
+        let json: serde_json::Value = if let Ok(json) = serde_json::from_reader(reader) {
+            json
+        } else {
+            continue;
+        };
+        println!("Parsing took: {:?}", start.elapsed());
+
+        let start = Instant::now();
+        let array = if let Some(array) = json.as_array() {
+            array
+        } else {
+            continue;
+        };
+        let mapped: Vec<StreamingDataRaw> = array
             .iter()
             .map(|x| {
                 serde_json::from_value(x.clone())
                     .unwrap_or_else(|_| panic!("Wasn't able to parse {x}"))
             })
+            .filter(|x: &StreamingDataRaw| x.is_song())
             .collect();
+        println!("Mapping took: {:?}", start.elapsed());
 
         all_streams.extend(mapped);
-        println!("Parsed file: {:?}", path);
+        // println!("Parsed file: {:?}", path);
         println!("Current length: {}", all_streams.len());
     }
     // dbg!(&all_streams[0]);
     dbg!(all_streams.len());
     // dbg!(json);
+
+    println!("Overall parsing took: {:?}", start_overall.elapsed());
 
     let total_time: u64 = all_streams
         .iter()
@@ -63,6 +84,75 @@ fn main() {
     let duration = chrono::Duration::milliseconds(total_time as i64);
 
     println!("Total stream time in hours: {}", duration.num_hours());
+
+    // let start = Instant::now();
+    // let mapped_streams = all_streams
+    //     .iter()
+    //     .map(|x| x.try_into())
+    //     .collect::<Vec<Result<StreamingData, ()>>>();
+    // println!("Mapping took: {:?}", start.elapsed());
+
+    // let start = Instant::now();
+    // let mut mapped_streams = mapped_streams
+    //     .into_iter()
+    //     .filter(|x| x.is_ok())
+    //     .map(|res| res.expect("Err values should have been filtered"))
+    //     .collect::<Vec<StreamingData>>();
+    // println!("Filtering took: {:?}", start.elapsed());
+
+    // This should achieve the same as the stuff above
+    let mut mapped_streams = all_streams
+        .iter()
+        .flat_map(|x| x.try_into())
+        .collect::<Vec<StreamingData>>();
+
+    let track_vec = all_streams
+        .iter()
+        .flat_map(|x| x.try_into())
+        .collect::<Vec<Track>>();
+
+    // mapped_streams.sort_by(|a, b| a.ts.cmp(&b.ts));
+
+    // println!("Mapped stream: {:?}", mapped_streams[0]);
+
+    let duration_vec = all_streams
+        .iter()
+        .map(|x| Duration::from_millis(x.ms_played as u64))
+        .collect::<Vec<Duration>>();
+
+    let something_vec = zip(track_vec, duration_vec).collect::<Vec<(Track, Duration)>>();
+
+    // let track_set = all_streams
+    //     .iter()
+    //     .flat_map(|x| x.try_into())
+    //     .collect::<HashSet<Track>>();
+    // // let track_set =
+
+    // TODO: add the amount of streams as a value to the hashmap
+    let mut track_hashmap: HashMap<Track, Duration> = HashMap::new();
+
+    for (track, time) in something_vec.into_iter() {
+        if track_hashmap.contains_key(&track) {
+            if let Some(duration) = track_hashmap.get_mut(&track) {
+                // TODO: fix this so it compiles lmao
+                *duration += time;
+                // let _ = track_hashmap.insert(track, duration);
+            }
+        } else {
+            track_hashmap.insert(track, time);
+        }
+    }
+
+    let mut track_vec = track_hashmap
+        .into_iter()
+        .collect::<Vec<(Track, Duration)>>();
+    track_vec.sort_by(|a, b| b.1.cmp(&a.1));
+    // println!("Track vec: {:?}", &track_vec[..20]);
+    for (i, (track, duration)) in track_vec[..20].iter().enumerate() {
+        println!("Number {}: Track: {:?}, Duration: {:?}", i, track, duration);
+    }
+
+    Ok(())
     // let example = json!([
     //     {"ts":"2022-12-09T22:28:37Z","username":"swaggerboy02","platform":"android","ms_played":1596,"conn_country":"NL","ip_addr_decrypted":"89.205.128.220","user_agent_decrypted":null,"master_metadata_track_name":"Out of Time","master_metadata_album_artist_name":"The Weeknd","master_metadata_album_album_name":"Dawn FM","spotify_track_uri":"spotify:track:2SLwbpExuoBDZBpjfefCtV","episode_name":null,"episode_show_name":null,"spotify_episode_uri":null,"reason_start":"backbtn","reason_end":"backbtn","shuffle":false,"skipped":true,"offline":false,"offline_timestamp":1670624916,"incognito_mode":false},
     // ]);
@@ -91,4 +181,10 @@ fn main() {
     // dbg!(&reason_starts);
     // dbg!(&reason_ends);
     // dbg!(nulls);
+}
+
+fn get_directories() -> Option<Vec<PathBuf>> {
+    let directory = Path::new("./data/");
+    let entries = fs::read_dir(directory).ok()?;
+    Some(entries.flatten().map(|entry| entry.path()).collect())
 }
--- src/streamingdata.rs
+++ src/streamingdata.rs
@@ -1,3 +1,4 @@
+use chrono::Utc;
 use serde::Deserialize;
 use std::option::Option;
 
@@ -52,7 +53,7 @@ pub enum EndReason {
 
 #[derive(Debug, Deserialize)]
 #[allow(unused)]
-pub struct StreamingData {
+pub struct StreamingDataRaw {
     // Timestamp in UTC
     // ts: chrono::DateTime<chrono::Utc>,
     ts: String, // quickly to make it work
@@ -100,4 +101,117 @@ pub struct StreamingData {
 
     // Indicate if the song was played in incognito mode
     incognito_mode: Option<bool>,
+}
+
+impl StreamingDataRaw {
+    pub fn is_song(&self) -> bool {
+        self.spotify_track_uri.is_some()
+    }
+}
+
+#[derive(Debug, Hash, Eq, PartialEq)]
+pub struct Track {
+    pub name: String,
+    pub artist: String,
+    pub album: String,
+    pub uri: String,
+}
+
+impl TryInto<Track> for &StreamingDataRaw {
+    type Error = ();
+
+    fn try_into(self) -> Result<Track, Self::Error> {
+        let name = self.master_metadata_track_name.clone().ok_or(())?;
+        // .inspect_err(|_| println!("track name not valid"))?;
+        let artist = self.master_metadata_album_artist_name.clone().ok_or(())?;
+        // .inspect_err(|_| println!("artist name not valid"))?;
+        let album = self.master_metadata_album_album_name.clone().ok_or(())?;
+        // .inspect_err(|_| println!("album name not valid"))?;
+        let uri_full = self.spotify_track_uri.clone().ok_or(())?;
+        let uri = uri_full.split(":").last().ok_or(())?.to_string();
+
+        // .inspect_err(|_| println!("track uri not valid"))?;
+
+        Ok(Track {
+            name,
+            artist,
+            album,
+            uri,
+        })
+    }
+}
+
+#[derive(Debug)]
+pub struct StreamingData {
+    pub ts: chrono::DateTime<chrono::Utc>,
+    ms_played: u32,
+    spotify_track_uri: String,
+    shuffle: Option<bool>,
+    offline: Option<bool>,
+    skipped: Option<bool>,
+    reason_start: StartReason,
+    reason_end: EndReason,
+}
+
+impl TryInto<StreamingData> for &StreamingDataRaw {
+    type Error = ();
+
+    fn try_into(self) -> Result<StreamingData, Self::Error> {
+        let reason_start = match &self.reason_start {
+            Some(reason) => match reason.as_str() {
+                "trackdone" => StartReason::Trackdone,
+                "backbtn" => StartReason::Backbtn,
+                "fwdbtn" => StartReason::Fwdbtn,
+                "playbtn" => StartReason::Playbtn,
+                "trackerror" => StartReason::Trackbrror,
+                "unknown" => StartReason::Unknown,
+                "appload" => StartReason::Appload,
+                "remote" => StartReason::Remote,
+                "clickrow" => StartReason::Clickrow,
+                "endplay" => StartReason::Endplay,
+                _ => return Err(()),
+            },
+            None => return Err(()),
+        };
+        let reason_end = match &self.reason_end {
+            Some(reason) => match reason.as_str() {
+                "fwdbtn" => EndReason::Fwdbtn,
+                "endplay" => EndReason::Endplay,
+                "trackerror" => EndReason::Trackerror,
+                "backbtn" => EndReason::Backbtn,
+                "unexpected-exit" => EndReason::UnexpectedExit,
+                "logout" => EndReason::Logout,
+                "trackdone" => EndReason::Trackdone,
+                "unexpected-exit-while-paused" => EndReason::UnexpectedExitWhilePaused,
+                "remote" => EndReason::Remote,
+                "unknown" => EndReason::Unknown,
+                _ => return Err(()),
+            },
+            None => EndReason::Unknown, // If we return Err here, there'll be quite a few errors
+        };
+        // let ts = chrono::DateTime::parse_from_rfc3339(&self.ts);
+        let ts_res = chrono::DateTime::parse_from_rfc3339(&self.ts);
+        let ts = ts_res
+            .inspect_err(|_| println!("Invalid datetime: {}", self.ts))
+            .map_err(|_| ())?
+            .with_timezone(&Utc);
+
+        // All errors are this one!!
+        let spotify_track_uri = self
+            .spotify_track_uri
+            .clone()
+            .ok_or(())
+            .inspect_err(|_| println!("track uri not valid?"))?;
+
+        Ok(StreamingData {
+            ts,
+            ms_played: self.ms_played,
+            spotify_track_uri,
+            shuffle: self.shuffle,
+            offline: self.offline,
+            skipped: self.skipped,
+            reason_start,
+            reason_end,
+        })
+    }
 }
